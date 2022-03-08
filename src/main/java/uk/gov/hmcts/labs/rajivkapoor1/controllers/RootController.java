a138a8e8a66a829c817b4718b433d80303da6a90
Enda Kelly <enda.kelly@hmcts.net>
2022-03-08 17:05:24 +0000

    more updates

--- src/main/java/uk/gov/hmcts/labs/rajivkapoor1/controllers/RootController.java
+++ src/main/java/uk/gov/hmcts/labs/rajivkapoor1/controllers/RootController.java
@@ -1,5 +1,8 @@
 package uk.gov.hmcts.labs.rajivkapoor1.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +15,15 @@ import static org.springframework.http.ResponseEntity.ok;
 @RestController
 public class RootController {
 
-    @GetMapping("/")
+        @Operation(summary = "Get welcome api",
+        description = "This is a welcome endpoint"
+        )
+
+        @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "A welcome message"),
+            @ApiResponse(responseCode = "404", description = "No welcome could be found")
+
+    @RequestMapping("/", method = GET, produces = TEXT_PLAIN_VALUE))
     public ResponseEntity<String> welcome() {
         return ok("Welcome to labs-rajivkapoor1 application");
     }
