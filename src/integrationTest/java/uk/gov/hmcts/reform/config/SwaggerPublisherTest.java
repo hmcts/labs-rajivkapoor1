13ba811a19402e3dec7added726d1f73d4422e0e
Enda Kelly <enda.kelly@hmcts.net>
2022-03-08 16:26:45 +0000

    more updates

--- src/integrationTest/java/uk/gov/hmcts/reform/config/SwaggerPublisherTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/config/SwaggerPublisherTest.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.labs.rajivkapoor1.config;
 
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
