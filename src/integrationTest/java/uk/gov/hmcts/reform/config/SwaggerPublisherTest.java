85f5e188fe1336a9b6789a60982c4de209aed017
Enda Kelly <enda.kelly@hmcts.net>
2022-03-09 09:24:32 +0000

    more updates

--- src/integrationTest/java/uk/gov/hmcts/reform/config/SwaggerPublisherTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/config/SwaggerPublisherTest.java
@@ -13,6 +13,7 @@ import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -30,16 +31,17 @@ class SwaggerPublisherTest {
 
     @DisplayName("Generate swagger documentation")
     @Test
-
     void generateDocs() throws Exception {
         byte[] specs = mvc.perform(get("/v3/api-docs"))
             .andExpect(status().isOk())
             .andReturn()
             .getResponse()
+            .getContentAsByteArray();
 
         try (OutputStream outputStream = Files.newOutputStream(Paths.get("/tmp/swagger-specs.json"))) {
             outputStream.write(specs);
         }
         assertEquals("true","true", "Foo equals bar");
+
     }
 }
